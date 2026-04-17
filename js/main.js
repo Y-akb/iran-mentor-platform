6392bd7d4ed6caa1f8358f6c1ee71bc8e540afef
Y-akb <123873068+Y-akb@users.noreply.github.com>
2026-04-17 12:22:56 +0100

    complete mobile menu with hamburger to X animation

--- js/main.js
+++ js/main.js
@@ -2,4 +2,15 @@ const menuToggle=document.querySelector('.menu-toggle');
 const nav=document.querySelector('.nav');
 menuToggle.addEventListener('click',()=>{
     nav.classList.toggle('active');
-})
+    menuToggle.classList.toggle('active');
+});
+//close menu when clicking outside
+document.addEventListener('click',(event)=>{
+    if(
+        !nav.contains(event.target)&&
+        !menuToggle.contains(event.target)
+    ){
+        nav.classList.remove('active');
+        menuToggle.classList.toggle('active');
+    }
+});
